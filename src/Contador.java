328fd3e863c4fe8ca27fe66925c5e10e0c04cc43
Rfiuza <rogerio.barbosa.fiuza@gmail.com>
2023-09-02 17:55:02 -0300

    desafio-dio

---
+++ src/Contador.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+    System.out.println("------------------------------------");    
+    System.out.println("     Desafio DIO - Contador         ");
+    System.out.println("------------------------------------");  
+    System.out.println("                                    ");  
+    
+    try (Scanner terminal = new Scanner(System.in)) {
+        System.out.print("Digite o primeiro parâmetro: ");
+        int parametroUm = terminal.nextInt();
+        System.out.print("Digite o segundo parâmetro: ");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+
+        for (int x = 1; x <= contagem; x++) {
+            System.out.println("Imprima o número: " + x);
+        }
+
+    }
+}    
